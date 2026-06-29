31728683108d4b885f181b414231f38662a2c04d
Karl Justine Robin <karljustiner@gmail.com>
2026-06-30 00:08:59 +0800

    feat: build end-to-end news data platform with dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,7 +1,29 @@
 import streamlit as st
+import requests
+import pandas as pd
 
-st.title("News Data Platform")
+st.set_page_config(page_title="News Data Platform")
 
-st.success("Platform is running!")
+st.title("📰 News Data Platform")
 
-st.write("🚀 Day 2 - Docker Infrastructure")
+st.write("Article Summary")
+
+response = requests.get(
+    "http://news-api:8000/article-summary"
+)
+
+if response.status_code == 200:
+
+    data = response.json()
+
+    df = pd.DataFrame(data)
+
+    st.dataframe(df)
+
+    st.bar_chart(
+        df.set_index("country")["article_count"]
+    )
+
+else:
+
+    st.error("Unable to connect to API")
